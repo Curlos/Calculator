59b6c4e7a667b945fde992f2e2017024615876e3
Carlos Martinez <curlosmart@gmail.com>
2021-08-04 12:34:58 -0400

    grid fixed and basic button functionality added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const addButton = document.querySelector('.add')
+const minusButton = document.querySelector('.subtract')
+const multiplyButton = document.querySelector('.multiply')
+const divideButton = document.querySelector('.divide')
+const buttons = document.getElementsByTagName('button')
+let resultElem = document.querySelector('.calculator-screen')
+let currentValue = Number(resultElem.value)
+let oldValue = null
+
 const add = (num1, num2) => {
     return num1 + num2
 }
@@ -14,15 +23,51 @@ const divide = (num1, num2) => {
     return num1 / num2
 }
 
+const clear = () => {
+    resultElem.value = '0'
+    oldValue = null
+}
+
+const plusMinus = (num) => {
+    currentValue = -1 * num
+    resultElem.value = String(currentValue)
+}
+
+const onePercentOfNum = (num) => {
+    currentValue = 0.01 * num
+    resultElem.value = String(currentValue)
+}
+
 const operators = {
     "add": add,
     "subtract": subtract,
     "multiply": multiply,
     "divide": divide,
+    "clear": clear,
+    "+/-": plusMinus,
+    "%": onePercentOfNum,
 }
 
 const operate = (operator, num1, num2) => {
     return operators[operator](num1, num2)
 }
 
+const handleButtonClick = (event) => {
+    const value = event.target.value
+    if (!isNaN(value)) {
+        currentValue = String(currentValue) + String(value)
+        resultElem.value = currentValue
+        currentValue = Number(currentValue)
+        console.log(currentValue)
+    } else if (value == "clear") {
+        operators["clear"]()
+    } else {
+        operators[value](currentValue)
+    }
+}
+
+for (let button of buttons) {
+    button.addEventListener('click', handleButtonClick)
+}
 console.log(operate("multiply", 50, 50))
+console.log(currentValue)
